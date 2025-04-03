33ad58854ea7c2821394f28765097aef4aa46cf7
Nevet21 <lubierarango@gmail.com>
2025-04-03 01:17:52 -0500

    poner los barcos de la maquina y poner los barcos del jugador mediante los dragables

--- src/JavaScript/app.js
+++ src/JavaScript/app.js
@@ -1,94 +1,154 @@
 document.addEventListener("DOMContentLoaded", function () {
-const contenedorTablero= document.querySelector('#contenedorTableros')
-const contenedorBarco= document.querySelector('.opcionBarco'); //recibe el div que contiene todos los div coon los barocs
-const botonGirar = document.querySelector('#girar-button');
-
-
-let angle=0
-function girar(){
-const opcionBarcos=(Array.from(contenedorBarco.children)) //se obtienen los barcos y se guarda en un arreglo
-    if (angle===0){ //girar los barcos dado el angulo, si es 90 a o si es 0 a 90
-        angle=90
-    } else{
-        angle=0
-    }
-    opcionBarcos.forEach(opcionBarcos => opcionBarcos.style.transform = `rotate(${angle}deg)`) 
-    }
-
-    botonGirar.addEventListener("click", girar)
-
-    let tamaño = 15;
-    const tableroSize = 200; // Tamaño fijo del tablero en píxeles
+    const contenedorTablero= document.querySelector('#contenedorTableros')
+    const contenedorBarco= document.querySelector('.opcionBarco'); //recibe el div que contiene todos los div coon los barocs
+    const botonGirar = document.querySelector('#girar-button');
     
-    function crearTablero(color, user) {
-        const tablero = document.createElement('div');
-        tablero.classList.add('tablero');
-        tablero.style.backgroundColor = color;
-        tablero.id = user;
     
-        // Calcular el tamaño de los bloques
-        const bloqueSize = `${tableroSize / tamaño}px`;
-    
-        // Establecer la variable CSS en el tablero
-        tablero.style.setProperty('--bloque-size', bloqueSize);
-    
-        for (let i = 0; i < tamaño * tamaño; i++) {
-            const bloque = document.createElement('div');
-            bloque.classList.add('bloque');
-            bloque.id = i;
-            tablero.append(bloque);
-        }
-    
-        contenedorTablero.append(tablero);
-    }
-    
-crearTablero('blue', 'jugador')
-crearTablero('red',   'maquina')
-
-// crear barcos
-
-class barco {
-    constructor(nombre,tamaño){
-        this.nombre=nombre
-        this.tamaño=tamaño
-
-    }
-
-}
-
-const destructor = new barco("destructor",2)
-const crucero = new barco("destructor",3)
-const submarino = new barco("destructor",3)
-const acorazado = new barco("destructor",4)
-const portaAviones = new barco("destructor",5)
-
-const barcos=[destructor,crucero,submarino,acorazado,portaAviones]
-
-function añadirBarco(barco){
-    const bloquesComputadora=document.querySelectorAll('#computer  div')
-    let randomBoolean= Math.random() < 0.5 //genera un aleatorio del 50 por ciento de las veces entre horizontal y vertical
-    let horizotal=randomBoolean
-    let randomIndex=Math.floor(Math.random()* tamaño*tamaño) //genera un random aleatorio en cualquier index del tablero para poner el inicio del barco
-    
-
-    let bloquesBarco=[]
-
-    for (let i = 0; i < barco.length; i++) {
-        if (horizontal) {
-            bloquesBarco.push(bloquesComputadora[Number(randomIndex)+ i]) //usamos un ciclo para agregar los bloques del barco a un arreglo, en este caso me encuentra los numero consecutivos al lado
-
+    let angle=0
+    function girar(){
+    const opcionBarcos=(Array.from(contenedorBarco.children)) //se obtienen los barcos y se guarda en un arreglo
+        if (angle===0){ //girar los barcos dado el angulo, si es 90 a o si es 0 a 90
+            angle=90
         } else{
-            bloquesBarco.push(bloquesComputadora[Number(randomIndex)+i*tamaño])//aqui encontramos los numeros de los bloques del barco vertical los cuales quedan justo debajo del numero inicial random
+            angle=0
+        }
+        opcionBarcos.forEach(opcionBarcos => opcionBarcos.style.transform = `rotate(${angle}deg)`) 
+        }
+    
+        botonGirar.addEventListener("click", girar)
+    
+        let tamaño = 10;
+        const tableroSize = 200; // Tamaño fijo del tablero en píxeles
+        
+    function crearTablero(color, user) {
+            const tablero = document.createElement('div');
+            tablero.classList.add('tablero');
+            tablero.style.backgroundColor = color;
+            tablero.id = user;
+        
+            // Calcular el tamaño de los bloques
+            const bloqueSize = `${tableroSize / tamaño}px`;
+        
+            // Establecer la variable CSS en el tablero
+            tablero.style.setProperty('--bloque-size', bloqueSize);
+        
+            for (let i = 0; i < tamaño * tamaño; i++) {
+                const bloque = document.createElement('div');
+                bloque.classList.add('bloque');
+                bloque.id = i;
+                tablero.append(bloque);
+            }
+        
+            contenedorTablero.append(tablero);
         }
         
+    crearTablero('yellow', 'player')
+    crearTablero('pink',   'computer')
+    
+    // crear barcos
+    
+    class barco {
+        constructor(nombre,tamaño){
+            this.nombre=nombre
+            this.tamaño=tamaño
+    
+        }
+    
     }
+    
+    const destructor = new barco("destructor",2)
+    const crucero = new barco("submarino",3)
+    const submarino = new barco("crucero",3)
+    const acorazado = new barco("acorazado",4)
+    const portaAviones = new barco("portaAviones",5)
+    
+    const barcos=[destructor,submarino,crucero,acorazado,portaAviones]
+    let notDropped
+
+    function añadirBarco(user,barco, startId){
+        const BloquesTablero= document.querySelectorAll(`#${user} div`)
+        let randomBoolean = Math.random() < 0.5
+        let isHorizontal = user=== 'player' ? angle===0 : randomBoolean
+        let randomStartIndex = Math.floor(Math.random()*tamaño*tamaño)
         
-    bloquesBarco.forEach(bloqueBarco=> {
-        bloqueBarco.classList.add(barco.nombre)
-        bloqueBarco.classList.add('token')
-    })
-}   
-añadirBarco(destructor)
+        let startIndex = startId ? startId : randomStartIndex
+
+        let validStart = isHorizontal ? startIndex <= tamaño*tamaño - barco.tamaño ? startIndex  :
+            tamaño*tamaño - barco.tamaño  :  
+            //vertica
+            startIndex <= tamaño*tamaño - tamaño * barco.tamaño ? startIndex: 
+                startIndex -  barco.tamaño *  tamaño  + tamaño
+
+        let bloquesBarco = []
+
+        for(let i=0; i < barco.tamaño; i++ ){
+            if (isHorizontal) {
+                bloquesBarco.push((BloquesTablero[Number(validStart)+i]))
+            }
+            else{
+                bloquesBarco.push(BloquesTablero[Number(validStart)+i*tamaño])
+
+            }
+        }
+
+        let valid
+
+        if ( isHorizontal){
+        bloquesBarco.every((_bloqueBarco, index)=>
+            valid= bloquesBarco[0].id % tamaño !== tamaño - (bloquesBarco.length - (index+1)))
+        }else{
+            bloquesBarco.every((_bloqueBarco,index)=>
+                valid=bloquesBarco[0].id < 90 + (tamaño* index+1))
+
+        }
+
+        const notTaken =bloquesBarco.every(bloqueBarco => !bloqueBarco.classList.contains('taken'))
+
+        if (valid && notTaken){
+            bloquesBarco.forEach(bloqueBarco =>{
+                bloqueBarco.classList.add(barco.nombre)
+                bloqueBarco.classList.add('taken')
+            })
+        } else{
+            if (user === 'computer') añadirBarco('computer',barco,null)
+            if (user=== 'player') notDropped = true
+        }
+        
+            
+    }
+    barcos.forEach(barco=> añadirBarco('computer',barco,null))
+
+    //barcos arrastrables
+    let barcoMovido
+    const opcionBarco = Array.from(contenedorBarco.children)
+    opcionBarco.forEach(opcion => opcion.addEventListener('dragstart', dragStart))
+
+    const bloquesJugador= document.querySelectorAll('#player div')
+        bloquesJugador.forEach(bloque => {
+            bloque.addEventListener('dragover', dragOver)
+            bloque.addEventListener('drop', dropShip)
+        })
+
+        function dragStart(e) {
+            notDropped = false;
+            barcoMovido = e.target;
+            console.log(barcoMovido);
+        }
+        
+
+    function dragOver(e){
+        e.preventDefault()
+    }
+
+    function dropShip(e){
+        const startId=e.target.id
+        const barco = barcos.find(b => b.nombre === barcoMovido.dataset.nombre);
+        añadirBarco('player',barco, startId)
+        if(!notDropped){
+            barcoMovido.remove()
+        }
+    }
 
 
-})
+ //fin   
+})  
